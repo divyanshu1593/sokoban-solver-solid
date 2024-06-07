52617ac0092dd62753435ee09900d28472649a02
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-06-07 17:16:48 +0530

    instanciated heuristics

--- src/singletons.ts
+++ src/singletons.ts
@@ -2,8 +2,11 @@ import { Display } from './board/display';
 import { Move } from './board/move';
 import { StateEvaluation } from './board/state-evaluation';
 import { StateHashing } from './common/state-hashing';
+import { AwayFromEmptyBoxHeuristics } from './search/away-from-empty-box-heuristic';
 import { BoxAtValidPlaceHeuristic } from './search/box-at-valid-place-heuristic';
+import { BoxAverageDistanceHeuristic } from './search/box-average-distance-heuristic';
 import { HeuristicEvaluation } from './search/heuristic-evaluation';
+import { SameDirectionHeuristic } from './search/same-direction-heuristic';
 import { Search } from './search/search';
 
 export const displayInstance = new Display();
@@ -11,7 +14,15 @@ export const animationInstance = new Animation();
 export const stateEvaluationInstance = new StateEvaluation();
 export const moveInstance = new Move(stateEvaluationInstance);
 export const boxAtValidPlaceHeuristicInstance = new BoxAtValidPlaceHeuristic();
-export const heuristicEvaluationInstance = new HeuristicEvaluation();
+export const awayFromEmptyBoxHeuristicsInstance =
+  new AwayFromEmptyBoxHeuristics();
+export const boxAverageDistanceHeuristicInstance =
+  new BoxAverageDistanceHeuristic();
+export const sameDirectionHeuristicInstance = new SameDirectionHeuristic();
+export const heuristicEvaluationInstance = new HeuristicEvaluation(
+  stateEvaluationInstance,
+  moveInstance,
+);
 export const stateHashingInstance = new StateHashing();
 export const searchInstance = new Search(
   stateEvaluationInstance,
