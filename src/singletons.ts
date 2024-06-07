b51222e4944ae97822bdb201ef0b2a8fdb0312d3
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-06-07 16:14:36 +0530

    instanciated classes for singletons

--- src/singletons.ts
+++ src/singletons.ts
@@ -1,7 +1,21 @@
 import { Display } from './board/display';
 import { Move } from './board/move';
 import { StateEvaluation } from './board/state-evaluation';
+import { StateHashing } from './common/state-hashing';
+import { BoxAtValidPlaceHeuristic } from './search/box-at-valid-place-heuristic';
+import { HeuristicEvaluation } from './search/heuristic-evaluation';
+import { Search } from './search/search';
 
 export const displayInstance = new Display();
+export const animationInstance = new Animation();
 export const stateEvaluationInstance = new StateEvaluation();
 export const moveInstance = new Move(stateEvaluationInstance);
+export const boxAtValidPlaceHeuristicInstance = new BoxAtValidPlaceHeuristic();
+export const heuristicEvaluationInstance = new HeuristicEvaluation();
+export const stateHashingInstance = new StateHashing();
+export const searchInstance = new Search(
+  stateEvaluationInstance,
+  heuristicEvaluationInstance,
+  stateHashingInstance,
+  moveInstance,
+);
